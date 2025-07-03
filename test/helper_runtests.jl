6f056e3480c2a9c5e4935db9f2431c8319daf6dd
Shriram Srinivasan <shrirams@lanl.gov>
2025-07-03 01:32:16 -0600

    updated helper_run_tests

--- test/helper_runtests.jl
+++ test/helper_runtests.jl
@@ -2,11 +2,13 @@
 
 function _check_correctness(sol::Dict{String,Any}, exact_sol::Dict{String,Any})
 
-    entries = ["compressor_flow", "pipe_flow", "nodal_pressure"]  # can add more quantities here if required
+    entries = ["compressor_flow", "pipe_flow", "nodal_pressure", "control_valve_flow", "valve_flow", "resistor_flow", "loss_resistor_flow", "short_pipe_flow"]  # can add more quantities here if required
     for key in entries
-        for i in keys(sol[key])
-            # @show sol[key][i] - exact_sol[key][string(i)]
-            @test isapprox(sol[key][i], exact_sol[key][string(i)]; rtol = 1e-4)
+        if haskey(sol, key)
+            for i in keys(sol[key])
+                # @show sol[key][i] - exact_sol[key][string(i)]
+                @test isapprox(sol[key][i], exact_sol[key][string(i)]; rtol = 1e-4)
+            end
         end
     end
     return
